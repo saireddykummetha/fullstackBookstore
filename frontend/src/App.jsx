e684d31c9886e957cec2d4c5a189d7c1595812b2
saireddykummetha <kummethasaikumarreddy@gmail.com>
2026-01-04 22:03:24 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
  
   useEffect(()=>{
     (async()=>{
-      const res=await fetch('https://fullstackbookstore-3.onrender.com')
+      const res=await fetch('https://bookstore-2txs.onrender.com')
       const resdata=await res.json()
       console.log(resdata)
       dispatch(setDataproduct(resdata))
